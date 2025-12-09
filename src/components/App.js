6707ac081fbbb3173e70bbe50ced5c88e7a4cf39
Laxmikant Shinde <laxmikant251102@gmail.com>
2025-12-09 16:39:55 +0530

    task done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,55 @@
 
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './../styles/App.css';
+import Login from "./Login";
+import Playground from "./Playground";
+import PrivateRoute from "./PrivateRoute";
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = () => {
+    setIsAuthenticated(!isAuthenticated);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <Router>
+      <div className="main-container">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/playground">PlayGround</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {isAuthenticated ? (
+          <p>Logged in, Now you can enter Playground</p>
+        ) : (
+          <p>You are not authenticated, Please login first</p>
+        )}
+
+        <Switch>
+          <Route path="/login">
+            <Login isAuthenticated={isAuthenticated} onLogin={handleLogin} />
+          </Route>
+          <PrivateRoute
+            path="/playground"
+            component={Playground}
+            isAuthenticated={isAuthenticated}
+          />
+          <Route path="/">
+            <div>
+              <h2>Page not Found</h2>
+            </div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
